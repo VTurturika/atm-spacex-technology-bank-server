efefa11dd11ac6146cd6d0639aa37e45e765d526
Victor Turturika <v.turturika@gmail.com>
2016-04-15 00:55:43 +0300

    Implemented test-connection request

--- src/main/java/BankServer.java
+++ src/main/java/BankServer.java
@@ -12,6 +12,24 @@ public class BankServer {
 
         staticFileLocation("/public");
         get("/", (request, response) -> logoHTML);
+
+        //customer requests
+        post("/customer/check-pin", (request, response) -> "");
+        post("/customer/receive-cash", (request, response) -> "");
+        post("/customer/add-cash", (request, response) -> "");
+        post("/customer/get-balance", (request, response) -> "");
+        post("/customer/change-pin", (request, response) -> "");
+
+        //service worker requests
+        post("/service/test-connection", (request, response) -> "Ready");
+        post("/service/check-service-key", (request, response) -> "");
+        post("/service/create-account", (request, response) -> "");
+        post("/service/add-card", (request, response) -> "");
+        post("/service/get-blocked-cards", (request, response) -> "");
+        post("/service/block-card", (request, response) -> "");
+        post("/service/unblock-card", (request, response) -> "");
+
+
     }
 
     private static final String logoHTML = "<html><body><img src=\"./spacex-technology.jpg\" " +
